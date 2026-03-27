cb7072121bfc6fcba362fe1b7e647b0aea1a727b
kevkevin <oapallikunnel@gmail.com>
2026-03-27 12:51:12 -0500

    log: added isStandardAddressScript function

--- lib/core/utils/address_script_conversions.dart
+++ lib/core/utils/address_script_conversions.dart
@@ -8,6 +8,9 @@ class AddressScriptConversions {
     required bool isTestnet,
   }) async {
     try {
+
+      if (!isStandardAddressScript(scriptPubkey)) return null;
+
       final address = await bdk.Address.fromScript(
         script: bdk.ScriptBuf(bytes: scriptPubkey),
         network: isTestnet ? bdk.Network.testnet : bdk.Network.bitcoin,
@@ -15,15 +18,63 @@ class AddressScriptConversions {
 
       return address.asString();
     } catch (e) {
-      // This is expected for non-address scripts (e.g. OP_RETURN) and
-      // not really a bug so using fine instead of severe or warning to avoid
-      // flooding the logger with it.
-      log.fine(
-        'error converting scriptPubkey to address',
+      log.severe(
+        message: 'error converting scriptPubkey to address',
         error: e,
         trace: StackTrace.current,
       );
       return null;
     }
   }
+}
+
+bool isStandardAddressScript(Uint8List script) {
+  if (script.isEmpty) return false;
+
+  final len = script.length;
+
+  // OP_RETURN (provably unspendable)
+  if (script[0] == 0x6a) return false;
+
+  // P2PKH: OP_DUP OP_HASH160 <20> OP_EQUALVERIFY OP_CHECKSIG
+  if (len == 25 &&
+      script[0] == 0x76 &&
+      script[1] == 0xa9 &&
+      script[2] == 0x14 &&
+      script[23] == 0x88 &&
+      script[24] == 0xac){
+    return true;
+  }
+
+  // P2SH: OP_HASH160 <20> OP_EQUAL
+  if (len == 23 &&
+      script[0] == 0xa9 &&
+      script[1] == 0x14 &&
+      script[22] == 0x87){
+    return true;
+  }
+
+  // P2WPKH: OP_0 <20>
+  if (len == 22 &&
+      script[0] == 0x00 &&
+      script[1] == 0x14){
+    return true;
+  }
+
+  // P2WSH: OP_0 <32>
+  if (len == 34 &&
+      script[0] == 0x00 &&
+      script[1] == 0x20){
+    return true;
+  }
+
+  // P2TR: OP_1 <32>
+  if (len == 34 &&
+      script[0] == 0x51 &&
+      script[1] == 0x20){
+    return true;
+  }
+
+  log.fine('script type is non-standard');
+  return false;
 }
